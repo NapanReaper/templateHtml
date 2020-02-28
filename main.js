e7472858c36e218e3ffaa1dbef5695101d052fc8
DuyDTNSE62826 <duydtnse62826@fpt.edu.vn>
2020-02-28 14:51:38 +0700

    [DuyDTN] fix http-server and adding jsonHandler

---
+++ main.js
@@ -0,0 +1,78 @@
+/* Node.js static file web server */
+
+// Importing necessary modules 
+const http = require('http');
+const fs = require('fs');
+const json2File = require('./utils/jsonObject2File');
+
+// Port on which the server will create 
+const PORT = 1800;
+
+// Maps file extension to MIME types which 
+// helps browser to understand what to do 
+// with the file 
+// Creating a server and listening at port 1800 
+json2File.transfer("book");
+http.createServer((req, res) => {
+    console.log(`${req.method} : ${req.url}`)
+    if (req.url.indexOf('.html') != -1) { //req.url has the pathname, check if it conatins '.html'
+        fs.readFile(__dirname + '/template/views' + req.url, function (err, data) {
+            if (err) console.log(err);
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            res.end();
+        });
+    }
+    if (req.url.indexOf('.js') != -1 && req.url.indexOf('node_modules') == -1 && req.url.indexOf('.json') == -1) { //req.url has the pathname, check if it conatins '.js'
+        fs.readFile(__dirname + '/template/js' + req.url, function (err, data) {
+            if (err) console.log(err);
+            res.writeHead(200, { 'Content-Type': 'text/javascript' });
+            res.write(data);
+            res.end();
+        });
+    }
+    if (req.url.indexOf('.js') != -1 && req.url.indexOf('node_modules') != -1 && req.url.indexOf('.json') == -1) { //req.url has the pathname, check if it conatins '.js'
+        fs.readFile(__dirname + '/' + req.url, function (err, data) {
+            if (err) console.log(err);
+            res.writeHead(200, { 'Content-Type': 'text/javascript' });
+            res.write(data);
+            res.end();
+        });
+    }
+    if (req.url.indexOf('.css') != -1) { //req.url has the pathname, check if it conatins '.css'
+        fs.readFile(__dirname + '/template/css' + req.url, function (err, data) {
+            if (err) console.log(err);
+            res.writeHead(200, { 'Content-Type': 'text/css' });
+            res.write(data);
+            res.end();
+        });
+    }
+    if (req.url.indexOf('.png') != -1) { //req.url has the pathname, check if it conatins '.png'
+        fs.readFile(__dirname + '/template/images' + req.url, function (err, data) {
+            if (err) console.log(err);
+            res.writeHead(200, { 'Content-Type': 'image/png' });
+            res.write(data);
+            res.end();
+        });
+    }
+    if (req.url.indexOf('.pdf') != -1) { //req.url has the pathname, check if it conatins '.pdf'
+        fs.readFile(__dirname + '/' + req.url, function (err, data) {
+            if (err) console.log(err);
+            res.writeHead(200, { 'Content-Type': 'application/pdf' });
+            res.write(data);
+            res.end();
+        });
+    }
+    if (req.url.indexOf('.json') != -1) { //req.url has the pathname, check if it conatins '.json'
+        var pattern = /\?(.*)/g;
+        var sanitizePath = req.url.replace(pattern, '');
+        fs.readFile(__dirname + '/data' + sanitizePath, function (err, data) {
+            if (err) console.log(err);
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.write(data);
+            res.end();
+        });
+    }
+}).listen(PORT);
+
+console.log(`Server listening on port ${PORT}`); 
---
+++ utils/jsonObject2File.js
@@ -0,0 +1,30 @@
+// file system module to perform file operations
+const fs = require('fs');
+
+// json data
+var jsonData = '{"problems":[{"Id":"1","Name":"Bai 1","Contents":"abc.pdf","LOC":"200"},{"Id":"2","Name":"Bai 3","Contents":"abc.pdf","LOC":"200"}]}';
+
+
+// parse json
+var jsonObj = JSON.parse(jsonData);
+
+// stringify JSON Object
+var jsonContent = JSON.stringify(jsonObj);
+exports.transfer = (fileName) => {
+    console.log(jsonObj);
+    console.log(jsonContent);
+    let dirPath = 'data';
+    try {
+        //check if dirPath is exists.
+        fs.statSync(dirPath);
+    } catch (e) {
+        fs.mkdirSync(dirPath);
+    }
+    fs.writeFile(dirPath + `/${fileName}.json`, jsonContent, 'utf8', function (err) {
+        if (err) {
+            console.log("An error occured while writing JSON Object to File.");
+            return console.log(err);
+        }
+        console.log("JSON file has been saved.");
+    });
+}
